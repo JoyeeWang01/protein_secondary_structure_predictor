1cc40ac8fb50bf060d40facd5ee3c8f5ee7dd2fc
JoyeeWang01 <47191804+JoyeeWang01@users.noreply.github.com>
2019-05-05 23:17:31 -0400

    Update script.js

--- public_html/script.js
+++ public_html/script.js
@@ -2,8 +2,8 @@ var firstTime = true;
 var model;
 
 function openTab() {
-  document.getElementById("main").style.marginLeft = "20%";
-  document.getElementById("mySidebar").style.width = "20%";
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
   document.getElementById("mySidebar").style.display = "block";
   document.getElementById("openNav").style.display = 'none';
 }
